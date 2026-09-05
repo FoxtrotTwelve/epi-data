baadf5493b1d38aacedb48dc8cf7244bd9cac868
FoxtrotTwelve <n_e_painter@yahoo.com>
2026-09-05 14:32:32 +1200

    4 questions added and answered in console

--- project.py
+++ project.py
@@ -17,3 +17,32 @@ worldData_dupeColumnCleared['pop'] = worldData_dupeColumnCleared['pop'].mask(wor
 worldData_dupeColumnCleared['area_km2'] = worldData_dupeColumnCleared['area_km2'].mask(worldData_dupeColumnCleared['area_km2'] < 1000) #removes too low area, if any
 print(worldData_dupeColumnCleared)
 worldData_dupeColumnCleared.to_csv('worldData_cleanedOutput.csv', index=False)
+
+worldData_cleaned = worldData_dupeColumnCleared #Changing variable names. Keeping the previous names and work to show my process. I would clean this up and refactor later.
+
+
+#Which continent has the most countries in the data?
+continent_mode = worldData_cleaned['continent'].mode() #extracts the item with the most
+print("Continent with Most Countries: " + continent_mode[0])
+
+
+#Which region has the largest combined area in sq. km?
+regions_by_area = worldData_cleaned.groupby('region_un')['area_km2'].sum() #groups and sums the regions' area
+#print(regions_by_area)
+region_largest_area = regions_by_area.idxmax() #picks region with the most area
+print("Region (Not Continent) with the Largest Area: " + region_largest_area) #Russia appears to be listed as Europe - ok fair enough
+
+
+#Which country has the highest life expectancy?
+highest_life_exp = worldData_cleaned.loc[worldData_cleaned['lifeExp'].idxmax(), 'name_long'] #picks the value of one column after determining the highest value in another column
+print("Country with Highest Life Expectency: " + highest_life_exp)
+
+
+#Which subregion has the lowest / highest average GDP per capita?
+subregion_dataframe = worldData_cleaned.groupby('subregion')['gdpPercap'].mean()
+print(subregion_dataframe)
+subregion_max = subregion_dataframe.idxmax()
+subregion_min = subregion_dataframe.idxmin()
+print(f"Max: {subregion_max} (Average GDP: {subregion_dataframe.max()})")
+print(f"Min: {subregion_min} (Average GDP: {subregion_dataframe.min()})")
+
